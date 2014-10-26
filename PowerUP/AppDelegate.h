c805a3cd730717b8275dc3d38a214b482f89c786
Cyrus Huang <cyrushuang@WL-218-155.WPA.Claremont.Edu>
2014-10-26 16:56:56 -0700

    Initial Commit

---
+++ PowerUP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PowerUP
+//
+//  Created by Cyrus Huang on 10/26/14.
+//  Copyright (c) 2014 CS121F14S4_Team8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PowerUP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PowerUP
+//
+//  Created by Cyrus Huang on 10/26/14.
+//  Copyright (c) 2014 CS121F14S4_Team8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
