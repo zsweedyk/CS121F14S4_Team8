7472654afdb4bac52478fdbf94acbfc9c75b2ece
Cyrus Huang <cyrushuang@DHCP-60-126.ST.HMC.Edu>
2014-10-28 18:01:30 -0700

    add bomb class

---
+++ PowerUP/Bomb.h
@@ -0,0 +1,13 @@
+//
+//  Bomb.h
+//  PowerUP
+//
+//  Created by Cyrus Huang on 10/28/14.
+//  Copyright (c) 2014 CS121F14S4_Team8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Bomb : UIImageView
+
+@end
---
+++ PowerUP/Bomb.m
@@ -0,0 +1,21 @@
+//
+//  Bomb.m
+//  PowerUP
+//
+//  Created by Cyrus Huang on 10/28/14.
+//  Copyright (c) 2014 CS121F14S4_Team8. All rights reserved.
+//
+
+#import "Bomb.h"
+
+@implementation Bomb
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
