7a929855490d29f03af92372a7491cf7a8f61897
Daniel <Daniel@WL-213-77.WPA.Claremont.Edu>
2014-12-12 14:38:32 -0800

    Added language to settings view controller

--- PowerUP/SettingsViewController.m
+++ PowerUP/SettingsViewController.m
@@ -17,6 +17,7 @@
     
     // language control and buttons
     UISegmentedControl *_segmentControl;
+    IBOutlet UIImageView *background;
 }
 
 @end
@@ -27,11 +28,10 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    [self.view setBackgroundColor:[UIColor whiteColor]];
-    
     // set up sounds, segmented control, and buttons
     [self setUpSounds];
     [self setUpSegControl];
+    [self setBackground];
 }
 
 - (void) setUpSounds
@@ -49,11 +49,31 @@
     CGFloat buttonHeight = buttonWidth / 3;
     
     _segmentControl = [[UISegmentedControl alloc]initWithItems:@[@"English",@"español",@"中文"]];
-    
-    _segmentControl.frame = CGRectMake((frameWidth - buttonWidth) / 2, (frameHeight - buttonHeight * 4), buttonWidth, buttonHeight / 2);
+    [_segmentControl setTintColor:[UIColor colorWithRed:0 green:251/255.0 blue:175/255.0 alpha:1]];
+    _segmentControl.frame = CGRectMake((frameWidth - buttonWidth) / 2, (frameHeight - buttonHeight * 6), buttonWidth, buttonHeight / 2);
     [_segmentControl setSelectedSegmentIndex:self.mainLanguage];
     [_segmentControl addTarget:self action:@selector(segmentedControlValueDidChange:) forControlEvents:UIControlEventValueChanged];
     [self.view addSubview:_segmentControl];
+}
+
+- (void) setBackground
+{
+    switch (self.mainLanguage) {
+        case ENGLISH:
+            [background setImage:[UIImage imageNamed:@"BGsettings.png"]];
+            break;
+            
+        case SPANISH:
+            [background setImage:[UIImage imageNamed:@"BGsettingsSP.png"]];
+            break;
+            
+        case CHINESE:
+            [background setImage:[UIImage imageNamed:@"BGsettingsCH.png"]];
+            break;
+            
+        default:
+            break;
+    }
 }
 
 /*
@@ -77,14 +97,17 @@
     switch (choice) {
         case ENGLISH:
             self.mainLanguage = ENGLISH;
+            [self setBackground];
             break;
             
         case SPANISH:
             self.mainLanguage = SPANISH;
+            [self setBackground];
             break;
             
         case CHINESE:
             self.mainLanguage = CHINESE;
+            [self setBackground];
             break;
             
         default:
